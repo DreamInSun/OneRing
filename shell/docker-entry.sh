0a6b978197f25226aa89aaa751ca92bd73289a43
DreamInSun <yancy_chen@hotmail.com>
2016-01-11 21:24:48 +0800

    调试Sed指令
    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entry.sh
+++ shell/docker-entry.sh
@@ -20,15 +20,18 @@ echo MySQL connection is $MYSQL_CONN
 
 echo ========== Database Config ==========
 cd $CATALINA_BASE/webapps/onering/WEB-INF/classes/config
+ls
 
-sed -i "s/{$XDIAMOND_JDBC_URL}/$XDIAMOND_JDBC_URL/g" ./application.demo.properties
-sed -i "s/{$XDIAMOND_JDBC_USERNAME}/$XDIAMOND_JDBC_USERNAME/g" ./application.demo.properties
-sed -i "s/{$XDIAMOND_JDBC_PASSWORD}/$XDIAMOND_JDBC_PASSWORD/g" ./application.demo.properties
+echo on
+sed -i ‘s/{XDIAMOND_JDBC_URL}/$XDIAMOND_JDBC_URL/g’           ./application.demo.properties
+sed -i ‘s/{XDIAMOND_JDBC_USERNAME}/$XDIAMOND_JDBC_USERNAME/g’ ./application.demo.properties
+sed -i ‘s/{XDIAMOND_JDBC_PASSWORD}/$XDIAMOND_JDBC_PASSWORD/g’ ./application.demo.properties
 
-sed -i "s/{$XDIAMOND_LDAP_URL}/$XDIAMOND_LDAP_URL/g" ./application.demo.properties
-sed -i "s/{$XDIAMOND_LDAP_USERDN}/$XDIAMOND_LDAP_USERDN/g" ./application.demo.properties
-sed -i "s/{$XDIAMOND_LDAP_PASSWORD}/$XDIAMOND_LDAP_PASSWORD/g" ./application.demo.properties
-sed -i "s/{$XDIAMOND_LDAP_BASE}/$XDIAMOND_LDAP_BASE/g" ./application.demo.properties
+sed -i ‘s/{XDIAMOND_LDAP_URL}/$XDIAMOND_LDAP_URL/g’           ./application.demo.properties
+sed -i ‘s/{XDIAMOND_LDAP_USERDN}/$XDIAMOND_LDAP_USERDN/g’     ./application.demo.properties
+sed -i ‘s/{XDIAMOND_LDAP_PASSWORD}/$XDIAMOND_LDAP_PASSWORD/g’ ./application.demo.properties
+sed -i ‘s/{XDIAMOND_LDAP_BASE}/$XDIAMOND_LDAP_BASE/g’         ./application.demo.properties
+echo off
 
 cat application.product.properties
 
