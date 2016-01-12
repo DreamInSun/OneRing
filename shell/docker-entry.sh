a84b87cb0269079ed70a2d2ea457950238b0d24a
DreamInSun <yancy_chen@hotmail.com>
2016-01-12 15:55:13 +0800

    修改配置

--- shell/docker-entry.sh
+++ shell/docker-entry.sh
@@ -8,6 +8,7 @@ export CLASSPATH=/usr/local/tomcat/bin/bootstrap.jar:/usr/local/tomcat/bin/tomca
 
 export CONFIG_PATH=/usr/local/tomcat/conf/
 
+export APP_PROFILE=product
 
 echo ========== Edit Configuration ==========
 echo CATALINA_BASE is $CATALINA_BASE
@@ -16,22 +17,26 @@ echo CATALINA_HOME is $CATALINA_HOME
 echo Application Profile is $APP_PROFILE
 
 echo ========== Initialize Database ==========
-echo MySQL connection is $MYSQL_CONN
+
 
 echo ========== Database Configuration ==========
 cd $CATALINA_BASE/webapps/onering/WEB-INF/classes/config
-ls
 
-echo on
+echo XDIAMOND_JDBC_URL
 sed -i "s/{XDIAMOND_JDBC_URL}/$XDIAMOND_JDBC_URL/g"           ./application.product.properties
+echo XDIAMOND_JDBC_USERNAME
 sed -i "s/{XDIAMOND_JDBC_USERNAME}/$XDIAMOND_JDBC_USERNAME/g" ./application.product.properties
+echo XDIAMOND_JDBC_PASSWORD
 sed -i "s/{XDIAMOND_JDBC_PASSWORD}/$XDIAMOND_JDBC_PASSWORD/g" ./application.product.properties
 
+echo XDIAMOND_LDAP_URL
 sed -i "s/{XDIAMOND_LDAP_URL}/$XDIAMOND_LDAP_URL/g"           ./application.product.properties
+echo XDIAMOND_LDAP_USERDN
 sed -i "s/{XDIAMOND_LDAP_USERDN}/$XDIAMOND_LDAP_USERDN/g"     ./application.product.properties
+echo XDIAMOND_LDAP_PASSWORD
 sed -i "s/{XDIAMOND_LDAP_PASSWORD}/$XDIAMOND_LDAP_PASSWORD/g" ./application.product.properties
+echo XDIAMOND_LDAP_BASE
 sed -i "s/{XDIAMOND_LDAP_BASE}/$XDIAMOND_LDAP_BASE/g"         ./application.product.properties
-echo off
 
 
 echo ========== Product Configuration ==========
