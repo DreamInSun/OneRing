c751da2676702992679c5fd8ee082d9f9bbab08e
DreamInSun <yancy_chen@hotmail.com>
2016-01-12 16:43:33 +0800

    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entry.sh
+++ shell/docker-entry.sh
@@ -22,20 +22,13 @@ echo ========== Initialize Database ==========
 echo ========== Database Configuration ==========
 cd $CATALINA_BASE/webapps/onering/WEB-INF/classes/config
 
-echo XDIAMOND_JDBC_URL = $XDIAMOND_JDBC_URL
-echo $temp | sed -i "s#{XDIAMOND_JDBC_URL}#$XDIAMOND_JDBC_URL#g"           ./application.product.properties
-echo XDIAMOND_JDBC_USERNAME
-echo $temp | sed -i "s#{XDIAMOND_JDBC_USERNAME}#$XDIAMOND_JDBC_USERNAME#g" ./application.product.properties
-echo XDIAMOND_JDBC_PASSWORD
-echo $temp | sed -i "s#{XDIAMOND_JDBC_PASSWORD}#$XDIAMOND_JDBC_PASSWORD#g" ./application.product.properties
+sed -i "s#{XDIAMOND_JDBC_URL}#$XDIAMOND_JDBC_URL#g"           ./application.product.properties
+sed -i "s#{XDIAMOND_JDBC_USERNAME}#$XDIAMOND_JDBC_USERNAME#g" ./application.product.properties
+sed -i "s#{XDIAMOND_JDBC_PASSWORD}#$XDIAMOND_JDBC_PASSWORD#g" ./application.product.properties
 
-echo XDIAMOND_LDAP_URL
 sed -i "s#{XDIAMOND_LDAP_URL}#$XDIAMOND_LDAP_URL#g"           ./application.product.properties
-echo XDIAMOND_LDAP_USERDN
 sed -i "s#{XDIAMOND_LDAP_USERDN}#$XDIAMOND_LDAP_USERDN#g"     ./application.product.properties
-echo XDIAMOND_LDAP_PASSWORD
 sed -i "s#{XDIAMOND_LDAP_PASSWORD}#$XDIAMOND_LDAP_PASSWORD#g" ./application.product.properties
-echo XDIAMOND_LDAP_BASE
 sed -i "s#{XDIAMOND_LDAP_BASE}#$XDIAMOND_LDAP_BASE#g"         ./application.product.properties
 
 
@@ -44,4 +37,4 @@ cat application.product.properties
 
 
 echo ========== Start Application ==========
-#bash $CATALINA_BASE/bin/catalina.sh run
+bash $CATALINA_BASE/bin/catalina.sh run
